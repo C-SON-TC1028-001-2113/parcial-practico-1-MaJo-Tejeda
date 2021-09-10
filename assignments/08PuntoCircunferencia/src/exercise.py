2d28279eac6f90d3461545a22fce4b7a9857bc66
MaJo-Tejeda <88874353+MaJo-Tejeda@users.noreply.github.com>
2021-09-10 09:40:47 -0700

    Update exercise.py

--- assignments/08PuntoCircunferencia/src/exercise.py
+++ assignments/08PuntoCircunferencia/src/exercise.py
@@ -1,8 +1,26 @@
 
-
+import math 
 def main():
     # Escribe tu código abajo de esta línea
-   
+  
+ r = float(input('Introduce el radio: ')) 
+ x1 = float(input('Introduce x1: '))
+ y1 = float(input('Introduce y1: '))
+ x2 = float(input('Introduce x2: '))
+ y2 = float(input('Introduce y2: '))
+ 
+ st = (x2-x1)**2+(y2-y1)**2
+ dis = math.sqrt(st)
+ 
+ if dis < r:
+     print('DENTRO')
+     
+ if dis > r:
+     print('FUERA')
+
+ if dis == r: 
+     print('SOBRE')
+
 
 if __name__ == '__main__':
     main()
